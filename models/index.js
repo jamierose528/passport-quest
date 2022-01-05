544eb80cf771983215cf9d6533a50feadd49f73e
Annie Nguyen <anguyen.aln@gmail.com>
2022-01-05 13:55:01 -0800

    added code to models

--- models/index.js
+++ models/index.js
@@ -1,5 +1,29 @@
-const Blog= require('./Blog');
-const Food = require('./Food');
-const Packing = require('./Packing');
-const Trip= require('./Trip');
-const User = require('./User');
+const Blog= require('./blog');
+const Food = require('./food');
+const Packing = require('./packing');
+const Trip= require('./trip');
+const User = require('./user');
+
+User.hasMany(Trip, {
+    // Define the third table needed to store the foreign keys
+    through: {
+      model: Trip,
+      unique: false
+    },
+    // Define an alias for when data is retrieved
+    as: 'planned_trips'
+  });
+  
+  Location.belongsToMany(Traveller, {
+    // Define the third table needed to store the foreign keys
+    through: {
+      model: Trip,
+      unique: false
+    },
+    // Define an alias for when data is retrieved
+    as: 'location_travellers'
+  });
+  
+  module.exports = { Blog, Food, Packing, Trip, User };
+
+  
