e5b5db3a28d1de8b691b1d8fc35ec8c868fbc7e1
JLee <thishereisjenn@gmail.com>
2022-01-10 14:32:02 -0800

    working on handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -19,34 +19,6 @@ const loginFormHandler = async (event) => {
   }
 };
 
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
-  const username = document.querySelector("#username-signup").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
-  const firstName = document.querySelector("#first_name-signup").value.trim();///double check
-  const lastName = document.querySelector("#last_name-signup").value.trim();///
-
-  if (username && email && password && firstName && lastName) {
-    const response = await fetch("/api/users", {
-      method: "POST",
-      body: JSON.stringify({ username, email, password, firstName, lastName }),
-      headers: { "Content-Type": "application/json" },
-    });
-
-    if (response.ok) {
-      document.location.replace("/profile");
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
-
 document
   .querySelector(".login-form")
   .addEventListener("submit", loginFormHandler);
-
-document
-  .querySelector(".signup-form")
-  .addEventListener("submit", signupFormHandler);
