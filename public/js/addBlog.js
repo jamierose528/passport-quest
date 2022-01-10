830a47b5fffb8256e431c1f5799a14275f832093
Annie Nguyen <anguyen.aln@gmail.com>
2022-01-09 21:56:30 -0800

    added code to addblog,addtrip,viewblog,viewtrip

---
+++ public/js/addBlog.js
@@ -0,0 +1,15 @@
+const addBlog = async () => {
+    const response = await fetch("/api/users/addBlog", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    });
+  
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert(response.statusText);
+    }
+  };
+  
+  document.querySelector("#addBlog").addEventListener("click", addBlog);
+  
---
+++ public/js/viewTrip.js
@@ -0,0 +1,15 @@
+const viewTrip = async () => {
+    const response = await fetch("/api/users/viewTrip", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    });
+  
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert(response.statusText);
+    }
+  };
+  
+  document.querySelector("#viewTrip").addEventListener("click", viewTrip);
+  
